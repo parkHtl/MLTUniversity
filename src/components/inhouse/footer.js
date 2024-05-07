7788b37837d59120d7855549e435249b7ddbaad8
Senith Uthsara <senithkarunarathneu@gmail.com>
2024-05-07 22:21:14 +0530

    add layout

---
+++ src/components/inhouse/footer.js
@@ -0,0 +1,12 @@
+'use client';
+
+const Footer = () => {
+    return (
+        <div className={'w-full h-[200px] bg-red-900'}>
+
+            footer ek methnt enw
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/inhouse/navBar.js
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <div className={'w-full sticky h-[80px] bg-green-400'}>
+            Navbar ek methnt enw
+        </div>
+    )
+}
+
+export default NavBar;
