eeeb120001504ca661f835f8781b159a1962db56
Senith Uthsara <senithkarunarathneu@gmail.com>
2024-05-07 22:21:14 +0530

    add layout

---
+++ src/components/inhouse/content.js
@@ -0,0 +1,8 @@
+const MainContent = ({children}) => {
+    return   (
+        <main className="flex w-full p-[20px] h-[200vh]">
+            {children}
+        </main>
+    )
+}
+export  default MainContent
---
+++ src/components/inhouse/header.js
@@ -0,0 +1,8 @@
+
+const Header = ()=>{
+    return (
+        <div className={'w-full h-[50px] bg-red-900'}>Header ek methnt enw</div>
+    )
+}
+
+export default Header
