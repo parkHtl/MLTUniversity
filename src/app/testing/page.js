ec598ea226ce0d3b66325bef3f56a0684f061e96
Suresh1129 <2020cs026@stu.ucsc.cmb.ac.lk>
2024-05-08 21:30:37 +0530

    image list

--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,13 +1,18 @@
 import SimpleCard from '@/components/mui/SimpleCard'
+import ImageCard from '@/components/mui/ImageCard'
 import React from 'react'
-import {BentoGrid} from "@/components/inhouse/bentoGrid";
 
 function page() {
   return (
+    <>
+    
     <div> 
-      {/*<SimpleCard />*/}
-        <BentoGrid/>
+      {/* <SimpleCard /> */}
+    </div >
+    <div className=' m-5'>
+    <ImageCard />
     </div>
+    </>
   )
 }
 
