53b4211d32d465d717598a327d27de17d26e39fb
Suresh1129 <2020cs026@stu.ucsc.cmb.ac.lk>
2024-05-08 21:30:37 +0530

    image list

--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,11 +1,18 @@
 import SimpleCard from '@/components/mui/SimpleCard'
+import ImageCard from '@/components/mui/ImageCard'
 import React from 'react'
 
 function page() {
   return (
+    <>
+    
     <div> 
-      <SimpleCard />
+      {/* <SimpleCard /> */}
+    </div >
+    <div className=' m-5'>
+    <ImageCard />
     </div>
+    </>
   )
 }
 
