2639df76bea0e6952775ca1fc9a66f7a733fa0ba
Senith Uthsara <senithkarunarathneu@gmail.com>
2024-05-09 23:52:40 +0530

    fix component order in home

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,19 @@ import {Globe} from "@/components/acui/glob";
 import {GlobeDemo} from "@/components/inhouse/GlobalAnimation";
 import {InfiniteMovingCardsScroll} from "@/components/inhouse/infinityCard";
 import {HeroComponent} from "@/components/inhouse/HeroComponent";
+import {BentoGrid} from "@/components/inhouse/bentoGrid";
 
 export default function Home() {
   return (
       <div>
           <HeroComponent/>
+
           <div className={'flex mt-[50px]'}>
               <GlobeDemo/>
               <div className={' py-[50px] flex flex-col gap-[30px] justify-center font-bold'}>
                   <h2 className={'text-3xl'}>Global Community: Diverse Perspectives</h2>
-                  <p className={'text-gray-600'}>Embrace the rich tapestry of cultures, traditions, and viewpoints that converge
+                  <p className={'text-gray-600'}>Embrace the rich tapestry of cultures, traditions, and viewpoints that
+                      converge
                       within our university's global community.
                       Our institution proudly hosts students from around the world,
                       fostering an environment where diversity thrives and differences are celebrated.
@@ -23,12 +26,12 @@ export default function Home() {
                       vibrant campus experience.</p>
               </div>
           </div>
-          <div>
-              <InfiniteMovingCardsScroll/>
-          </div>
-
+          <BentoGrid/>
           <div>
               <ImageGridAndText/>
+          </div>
+          <div>
+              <InfiniteMovingCardsScroll/>
           </div>
           <div>
               <HowtoApply></HowtoApply>
