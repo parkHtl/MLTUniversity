d70ac574e740509f796e89f08a828afcfd4e3d73
Suresh1129 <2020cs026@stu.ucsc.cmb.ac.lk>
2024-05-09 20:27:43 +0530

    component_complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
+import HowtoApply from "@/components/inhouse/HowtoApply";
+import ImageGridAndText from "@/components/inhouse/ImageGridAndText";
 import Header from "@/components/inhouse/header";
 import SimpleCard from "@/components/mui/SimpleCard";
 
 export default function Home() {
   return (
     <main className="">
-      <SimpleCard></SimpleCard>
+      <div>
+
+      </div>
+      
+      <div>
+        <ImageGridAndText/>
+      </div>
+      <div>
+        <HowtoApply></HowtoApply>
+      </div>
+      
     </main>
   );
 }
--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,22 +1,20 @@
-import SimpleCard from '@/components/mui/SimpleCard'
-import ImageCard from '@/components/mui/ImageCard'
-import React from 'react'
-import {BentoGrid} from "@/components/inhouse/bentoGrid";
+import SimpleCard from "@/components/mui/SimpleCard";
+import ImageCard from "@/components/mui/ImageCard";
+import React from "react";
+import { BentoGrid } from "@/components/inhouse/bentoGrid";
+import AboutUs from "@/components/inhouse/AboutUs";
 
 function page() {
   return (
     <>
-
-    <div>
-      {/* <SimpleCard /> */}
-    </div >
-    <div className=' m-5'>
-    <ImageCard />
-      {/*<SimpleCard />*/}
-        <BentoGrid/>
-    </div>
+      <div>{/* <SimpleCard /> */}</div>
+      <div className=" m-5">
+        <ImageCard />
+        {/*<SimpleCard />*/}
+        <BentoGrid />
+      </div>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
