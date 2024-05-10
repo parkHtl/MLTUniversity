0555a869e8f9280aaf30c0281e91e48dbff6398d
Suresh1129 <2020cs026@stu.ucsc.cmb.ac.lk>
2024-05-11 02:08:25 +0530

    partner add

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import { GlobeDemo } from "@/components/inhouse/GlobalAnimation";
 import { InfiniteMovingCardsScroll } from "@/components/inhouse/infinityCard";
 import { HeroComponent } from "@/components/inhouse/HeroComponent";
 import { BentoGrid } from "@/components/inhouse/bentoGrid";
-import {StickyScrollReveal} from "@/components/inhouse/stickyScroll";
+import { StickyScrollReveal } from "@/components/inhouse/stickyScroll";
 import Partners from "@/components/inhouse/Partners";
 
 export default function Home() {
@@ -17,44 +17,40 @@ export default function Home() {
 
       <div className={"flex mt-[50px]"}>
         <GlobeDemo />
-        <div
-          className={
-            " py-[50px] flex flex-col gap-[30px] justify-center "
-          }
-        >
-          <h2 className={"text-3xl font-bold"}>FURTHER EDUCATION OPPORTUNITIES</h2>
-            <p className={"text-gray-600 text-[20px] leading-10 text-justify"}>
-                The PGC Diploma in MLT students could opt to transfer to any
-                healthcare related (Bio Medicine, Bio Technology, Nursing,
-                Microbiology, Biology Medical Laboratory Science, etc.) In
-                corresponding year of bachelors overseas in more than 60 University
-                partners in over 20 countries including and but not limiting to
-                Canada, UK, Australia, France, Singapore, Malaysia. Contact PATHE
-                Study Aboard for more information. www.pathestudyabroad.lk
-            </p>
+        <div className={" py-[50px] flex flex-col gap-[30px] justify-center "}>
+          <h2 className={"text-3xl font-bold"}>
+            FURTHER EDUCATION OPPORTUNITIES
+          </h2>
+          <p className={"text-gray-600 text-[20px] leading-10 text-justify"}>
+            The PGC Diploma in MLT students could opt to transfer to any
+            healthcare related (Bio Medicine, Bio Technology, Nursing,
+            Microbiology, Biology Medical Laboratory Science, etc.) In
+            corresponding year of bachelors overseas in more than 60 University
+            partners in over 20 countries including and but not limiting to
+            Canada, UK, Australia, France, Singapore, Malaysia. Contact PATHE
+            Study Aboard for more information. www.pathestudyabroad.lk
+          </p>
         </div>
       </div>
-        <div className={"my-[60px]"}>
-
+      <div className={"my-[60px]"}>
         <BentoGrid />
       </div>
       <div className="mt-16">
         <ImageGridAndText />
       </div>
-        <div>
-            <h2 className={"text-3xl font-bold mt-[50px]"}>FURTHER EDUCATION OPPORTUNITIES</h2>
-            <StickyScrollReveal/>
-        </div>
-        <div>
-            <HowtoApply></HowtoApply>
-        </div>
-      <div className="mt-16">
-        <HowtoApply></HowtoApply>
+      <div>
+        <h2 className={"text-3xl font-bold mt-[50px]"}>
+          FURTHER EDUCATION OPPORTUNITIES
+        </h2>
+        <StickyScrollReveal />
       </div>
+
       <div className="mt-16">
         <Partners />
       </div>
-      
+      <div className="mt-16">
+        <HowtoApply></HowtoApply>
+      </div>
     </div>
   );
 }
