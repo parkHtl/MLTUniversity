1a782127fd7f876341c58295ddf41ff7e21c9f77
Senith Uthsara <senithkarunarathneu@gmail.com>
2024-06-15 13:50:29 +0530

    download brochure

--- src/components/inhouse/Textcontent.js
+++ src/components/inhouse/Textcontent.js
@@ -12,7 +12,8 @@ export default function Textcontent() {
           Learn more about this General modules, Recognitions and Accreditation and more or download our Brochure.
         </p>
         <Link
-            href="#"
+            href="/docs/brochure.pdf"
+            download={'brochure.pdf'}
             className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
             prefetch={false}
         >
