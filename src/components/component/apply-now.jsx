5ac968457587d892dfaff09636e9472f86101c43
parkHtl <parkhtlmlt@gmail.com>
2024-12-19 10:22:56 +0530

    Update apply-now.jsx
    added google form apply now link

--- src/components/component/apply-now.jsx
+++ src/components/component/apply-now.jsx
@@ -26,7 +26,7 @@ export function ApplyNow() {
               Apply through our online portal for our undergraduate programs
             </p>
             <Link
-              href="https://panaska.pathe.lk/signin.php?pid=1"
+              href="https://forms.gle/gYyynf7GHraEnJNT6"
               className="mt-3 inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium
                text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1
                 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900
