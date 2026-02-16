7ef24bc8141ab9a11cb9762e877ddf3617c70fa3
parkHtl <parkhtlmlt@gmail.com>
2026-02-16 14:11:24 +0530

    Update schol-info.jsx
    changed scholorship url

--- src/components/component/schol-info.jsx
+++ src/components/component/schol-info.jsx
@@ -95,7 +95,7 @@ export function ScholInfo() {
       </p>
       <div className="flex gap-3 justify-center items-center mt-4 md:flex-row flex-col">
         <Link
-          href="https://forms.gle/LzYReR8UBV3V12qX9"
+          href="https://forms.gle/jSg5jWwYWGPUytuM8"
           className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
           prefetch={false}
         >
